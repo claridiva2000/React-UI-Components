116a70c5634c2e48123a36ac3472a6d5c07ccfd5
claridiva2000 <claridiva2000@gmail.com>
2019-02-28 01:27:35 -0600

    footer buttons

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -5,7 +5,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <div className="Footer">
-    <div className="stats"> <i className="speech" class="far fa-comment" onClick={comment}></i><span><i class="fas fa-sync-alt"></i>6</span> <span><i class="far fa-heart"></i>4</span><i class="far fa-envelope"></i>
+    <div className="stats"> <i id="speach" className="speech" class="far fa-comment" onClick={comment}></i><span><i id="reshare" class="fas fa-sync-alt"></i>6</span> <span ><i id="heart" class="far fa-heart"></i>4</span><i id="email" class="far fa-envelope"></i>
     </div>
     <div className="comment">
     <form>
