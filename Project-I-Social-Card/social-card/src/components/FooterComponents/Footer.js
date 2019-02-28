c9138a7cf0199e0b561f0ab5e7742114ad627db9
claridiva2000 <claridiva2000@gmail.com>
2019-02-28 01:16:57 -0600

    starting stretch

--- Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -5,11 +5,27 @@ import './Footer.css';
 const Footer = () => {
   return (
     <div className="Footer">
-    <div className="stats"> <i class="far fa-comment"></i><span><i class="fas fa-sync-alt"></i>6</span> <span><i class="far fa-heart"></i>4</span><i class="far fa-envelope"></i>
+    <div className="stats"> <i className="speech" class="far fa-comment" onClick={comment}></i><span><i class="fas fa-sync-alt"></i>6</span> <span><i class="far fa-heart"></i>4</span><i class="far fa-envelope"></i>
+    </div>
+    <div className="comment">
+    <form>
+    <textarea placeholder="What's on your mind?">
+    </textarea>
+    <button>
+      Submit
+    </button>
+    </form>
+    
     </div>
     </div>
   );
 };
+
+
+const commentform= document.getElementsByTagName("form");
+function comment(){
+  commentform.style.display ='flex';
+  };
 
 
 export default Footer;
--- Project-I-Social-Card/social-card/src/index.js
+++ Project-I-Social-Card/social-card/src/index.js
@@ -6,3 +6,14 @@ import App from './App';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// const commentform= document.querySelector(".comment");
+// const speak = document.querySelector('.speech');
+// speak.addEventListener('click', ()=>{
+//   if(commentform.style.display === 'flex') {
+//     commentform.style.display = 'none';
+//   }      
+//   else {
+//     commentform.style.display === 'flex';
+//   }      
+// });
