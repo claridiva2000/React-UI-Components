76677518f7b4e9db8e1bfd21e2b4ac3706fb8cea
claridiva2000 <claridiva2000@gmail.com>
2019-03-02 14:14:34 -0600

    calculator

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Button.css';
+
+const ActButtons = () => {
+  return (
+    <div className="ActButtons">
+    <div className="btnheight act">/</div>
+    <div className="btnheight act">X</div>
+    <div className="btnheight act">-</div>
+    <div className="btnheight act">+</div>
+    <div className="btnheight act">=</div>
+    </div>
+  );
+};
+
+
+export default ActButtons;
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Display.css';
+ 
+
+const Display = () => {
+  return (
+    <div className="Display btnheight">
+   3.145682
+    </div>
+  );
+};
+
+
+export default Display;
